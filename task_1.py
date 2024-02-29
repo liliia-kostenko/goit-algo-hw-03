95b6d190281e4a09bd70d596876739722694d9bf
Liliia Kostenko <liliia.kostenko17@gmail.com>
2024-02-29 22:32:15 +0100

    task_1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,24 @@
-print(2+2)
+from datetime import datetime
+
+now = datetime.now()
+
+# Форматування дати і часу
+formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_date) 
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%A, %d %B %Y")
+print(formatted_date_only)
+
+# Форматування лише часу
+formatted_time_only = now.strftime("%I:%M %p")
+print(formatted_time_only)  
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%d.%m.%Y")
+print(formatted_date_only)
+
+
+
+
+
